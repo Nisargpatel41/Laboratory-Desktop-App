6bfaaa855b0ebdcb2bd3df2b280bb6b9ace021dd
Nisarg Patel <nhp0640@gmail.com>
2021-03-29 16:05:22 +0530

    single reciept component created

---
+++ src/components/Receipt/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Receipt = () => {
+  return (
+    <>
+      <div className="border border-dark px-3">
+        <Header />
+        {/* Register Number and Date Row  */}
+        <Row>
+          <Col>
+            <label>R.No:</label>
+            <span className="ml-2">1234</span>
+          </Col>
+          <Col className="text-right">
+            <label>Date:</label>
+            <span className="ml-2">19/03/2021</span>
+          </Col>
+        </Row>
+
+        {/* Received From Row  */}
+        <Row className="mt-2">
+          <Col>
+            <span>Received From: </span>
+          </Col>
+          <Col>
+            <span>Bhadreshbhai Chandubhai Bhatia</span>
+          </Col>
+        </Row>
+
+        {/* Name Row  */}
+        <Row className="mt-2">
+          <Col>
+            <span>Name: </span>
+          </Col>
+          <Col>
+            <span>Bhadreshbhai Chandubhai Bhatia</span>
+          </Col>
+        </Row>
+
+        {/* Investigation Row  */}
+        <Row className="mt-2">
+          <Col>
+            <span>For Laboratory Investigation as Follows : </span>
+          </Col>
+          <Col>
+            <span>Tav, Khasi, Shardi, loose motion etc.</span>
+          </Col>
+        </Row>
+
+        {/* Price Row  */}
+        <Row className="mt-2">
+          <Col>
+            <span>Rs : </span>
+          </Col>
+          <Col>
+            <div className="d-flex flex-column">
+              <span>1400.00</span>
+              <span className="small">One Thousand Fourteen Hundred Only.</span>
+            </div>
+          </Col>
+        </Row>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Receipt;
--- src/components/ReceiptsContainer/index.js
+++ src/components/ReceiptsContainer/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import Receipt from "../Receipt";
 import SearchForm from "./SearchForm";
 
 const ReceiptsContainer = () => {
   return (
     <div className="p-4">
       <Row className="m-0 p-0">
-        <SearchForm />
+        <Col xs={12}>
+          <SearchForm />
+        </Col>
+      </Row>
+      <Row className="m-0 p-0">
+        <Col xs={6} className="pt-3 cursor-pointer">
+          <Receipt />
+        </Col>
+        <Col xs={6} className="pt-3">
+          <Receipt />
+        </Col>
+        <Col xs={6} className="pt-3">
+          <Receipt />
+        </Col>
       </Row>
     </div>
   );
